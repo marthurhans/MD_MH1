d757b9a9c45c06b696421737c82b76b482293741
Mike Hans <marthurhans@gmail.com>
2025-03-20 01:35:51 -0400

    add simple MainActivity layout and functionality to test emulator

--- app/src/main/java/com/mikehans/d308vacationplanner/MainActivity.java
+++ app/src/main/java/com/mikehans/d308vacationplanner/MainActivity.java
@@ -1,4 +1,24 @@
 package com.mikehans.d308vacationplanner;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "Vacation Planning Started!",
+                        Toast.LENGTH_SHORT).show()
+        );
+    }
 }
+
